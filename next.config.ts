1c40ec68bec13f59ed28d4242ce558b55573a0e2
TheRedstoneWolf <jannickhora@outlook.de>
2026-03-09 20:31:52 +0100

    Add Docker configuration and next.config standalone

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // Nötig für Docker-Deployment
 };
 
 export default nextConfig;
